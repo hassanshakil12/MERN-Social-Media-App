68deb82895f4fc4212b48b6da035281ac734cdc8
Muhammad Hassan <hassanshakil12@gmail.com>
2024-07-31 18:26:57 +0500

    Defined react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Feed from "./components/Feed/Feed.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Profile from "./components/Profile/Profile.jsx";
+import Register from "./components/Auth/Register.jsx";
+import Login from "./components/Auth/Login.jsx";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
