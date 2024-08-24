6395b708b31fa2a7a733af266bf643d9ab1a5636
Muhammad Hassan <hassanshakil12@gmail.com>
2024-08-24 19:50:51 +0500

    Added Loading state for Feed component

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -1,36 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { fetchPost, currentUserProfile } from "../../services/Api.jsx";
 import PostCard from "./PostCard.jsx";
-import "./Feed.css"
+import "./Feed.css";
 
 const Feed = () => {
   const [posts, setPosts] = useState([]);
   const [currentUser, setCurrentUser] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    try {
-      const getCurrentUser = async () => {
+    const getCurrentUser = async () => {
+      try {
         const response = await currentUserProfile();
         setCurrentUser(response.data);
-      };
-      const getPosts = async () => {
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const getPosts = async () => {
+      try {
         const response = await fetchPost();
         setPosts(response.data);
-      };
-      getPosts();
-      getCurrentUser();
-    } catch (error) {
-      console.error(error);
-    }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPosts();
+    getCurrentUser();
+
+    setLoading(true);
+    Promise.all([getPosts(), getCurrentUser()]).then(() => setLoading(false));
   }, []);
+
+  if (loading)
+    return (
+      <div
+        style={{ backgroundColor: "black", color: "white", fontSize: "50px" }}
+      >
+        loading
+      </div>
+    );
 
   console.log(posts);
 
   return (
     <div className="feed-container">
-      {posts.map((post) => (
-        <PostCard key={post._id} post={post} currentUser={currentUser} />
-      ))}
+      {posts
+        ? posts.map((post) => (
+            <PostCard key={post._id} post={post} currentUser={currentUser} />
+          ))
+        : loading}
     </div>
   );
 };
